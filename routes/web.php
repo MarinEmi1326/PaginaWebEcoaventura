9f63b77fc7b1e39d9ae66a091c8506cf9a902f87
MarinEmi1326 <laraemi1326@gmail.com>
2026-02-24 20:03:11 -0600

    Eliminacion de archivos

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Admin\AdminSitioController;
-use App\Http\Controllers\HoteleroController;
-use App\Http\Controllers\RestauranteroController;
 use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\Admin\AdminSolicitudesController;
 use Laravel\Socialite\Facades\Socialite;
@@ -39,16 +37,7 @@ Route::get('/destinos/{tipo}', function ($tipo) {
 ->whereIn('tipo', ['turisticos','ecoturisticos','balnearios'])
 ->name('destinos.tipo');
 
-// apenas se va a agregar de las vistas 
-Route::get('/servicios', function () {
-    return view('servicios.index');
-})->name('servicios.index');
 
-Route::get('/servicios/{tipo}', function ($tipo) {
-    return view('servicios.index', compact('tipo'));
-})
-->whereIn('tipo', ['hospedaje','restaurantes'])
-->name('servicios.tipo');
 
 Route::get('/mapa', function () {
     return view('mapa');
@@ -127,46 +116,6 @@ Route::middleware('auth')->group(function () {
         Route::post('/solicitudes/{id}/rechazar', [AdminSolicitudesController::class, 'rechazar'])
             ->name('solicitudes.rechazar');
 
-    });
-
-    /*
-    |--------------------------------------------------------------------------
-    | HOTELERO
-    |--------------------------------------------------------------------------
-    */
-    Route::prefix('hotelero')->name('hotelero.')->group(function () {
-
-        Route::get('/dashboard', [HoteleroController::class, 'dashboard'])->name('index');
-        Route::get('/reservas', [HoteleroController::class, 'reservas'])->name('reservas');
-        Route::get('/habitaciones', [HoteleroController::class, 'habitaciones'])->name('habitaciones');
-        Route::get('/servicios', [HoteleroController::class, 'servicios'])->name('servicios');
-        Route::get('/mi-hotel', [HoteleroController::class, 'miHotel'])->name('mi-hotel');
-        Route::post('/hotel/actualizar', [HoteleroController::class, 'update'])->name('hotel.update');
-        
-        Route::get('/reservas/crear', [HoteleroController::class, 'createReserva'])->name('reservas.create');
-        Route::post('/reservas/guardar', [HoteleroController::class, 'storeReserva'])->name('reservas.store');
-        Route::get('/hotel/editar', [HoteleroController::class, 'editHotel'])->name('hotel.edit');
-        Route::post('/hotel/suspender', [HoteleroController::class, 'suspenderHotel'])->name('hotel.suspender');
-
-        Route::get('/reservas/{id}', [HoteleroController::class, 'showReserva'])->name('reservas.show');
-        Route::post('/reservas/{id}/aprobar', [HoteleroController::class, 'aprobarReserva'])->name('reservas.aprobar');
-        Route::post('/reservas/{id}/rechazar', [HoteleroController::class, 'rechazarReserva'])->name('reservas.rechazar');
-        // Perfil
-        Route::get('/perfil', [HoteleroController::class, 'perfil'])->name('perfil');
-        Route::put('/perfil', [HoteleroController::class, 'updatePerfil'])->name('perfil.update');
-    });
-
-    /*
-    |--------------------------------------------------------------------------
-    | RESTAURANTERO
-    |--------------------------------------------------------------------------
-    */
-    Route::prefix('restaurantero')->name('restaurantero.')->group(function () {
-
-        Route::get('/dashboard', [RestauranteroController::class, 'dashboard'])->name('dashboard');
-        Route::get('/menu', [RestauranteroController::class, 'menu'])->name('menu');
-        Route::get('/reservas', [RestauranteroController::class, 'reservas'])->name('reservas');
-        Route::get('/opiniones', [RestauranteroController::class, 'opiniones'])->name('opiniones');
     });
 
 });
